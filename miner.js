805b35a44b8cb8c7df515b38358a5b502b4a3acd
Konstantin Farrell <konstantinfarrell@gmail.com>
2016-05-30 05:02:05 -0700

    Update miner demo script

--- miner.js
+++ miner.js
@@ -1,18 +1,40 @@
+// Get current npc coords
 var x = Math.floor(npc.getBlockX());
 var y = Math.floor(npc.getBlockY());
 var z = Math.floor(npc.getBlockZ());
 
-var distance = 30;
-var height = y+1;
+// Define some helpful variables
+var sight = 50;
 var closestDistance = 99999999;
+var h = Number(y)+1;
+var coordinates = [];
+var block=null;
 
-for(i =0; i<distance; i++){
-    x = Math.floor(npc.getBlockX()) - (distance/2) + i;
-    for(j=0; j<distance; j++){
-        z = Math.floor(npc.getBlockZ()) - (distance/2) + j;
-        var blockToCheck = world.getBlock(x,height,z);
-        npc.say(blockToCheck);
+// Loop in a big square around the npc
+// looking for the closest stone
+for(i=0; i<sight; i++){
+    var block_x = Math.floor(npc.getBlockX()-(sight/2) + i);
+    for(j=0; j<sight; j++){
+        var block_z = Math.floor(npc.getBlockZ()-(sight/2) + j);
+        // Grab the block we're checking
+        var checkblock = world.getBlock(block_x,h,block_z);
+        if(checkblock != null){
+            // Compare its distance to the previous closest
+            // block and keep the closer of the two.
+            var distance = Math.sqrt(Math.pow(Math.abs(block_z-z), 2)+Math.pow(Math.abs(block_x-x), 2));
+            if(distance < closestDistance){
+                if(checkblock.getName() == "minecraft:stone"){
+                    closestDistance = distance;
+                    coordinates = [block_x,h,block_z];
+                    block = checkblock;
+                }
+            }
+        }
     }
 }
+
+// Prevent the default action from occurring.
 event.setCancelled(true);
-npc.navigateTo(x+5, y, z, 1);
+
+// Walk over to the block.
+npc.navigateTo(coordinates[0], coordinates[1], coordinates[2], 1);
